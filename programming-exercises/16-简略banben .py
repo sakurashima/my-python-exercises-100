db1f167fa628a3b3e0064360fac869f15fd979ed
sakurashima <kunqian888#outlook.com>
2020-09-13 18:45:49 +0800

    100

--- programming-exercises/16-简略banben .py
+++
@@ -1,9 +0,0 @@
-def main():
-"""运行不了!"""
-
-    values = input()
-    numbers = [x for x in values.split(",") if int(x) % 2 != 0]
-    print(",".join(numbers))
-
-if __name__ == "__main__":
-    main()
